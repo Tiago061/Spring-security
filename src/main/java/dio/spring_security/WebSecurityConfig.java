7673f6a0bd92b73b0d2aa1b9df92c645ff5435f9
Tiago061 <Thenriqueleite277@gmail.com>
2024-09-05 00:24:40 -0300

    Autenticação e Autorização com Spring Security

---
+++ src/main/java/dio/spring_security/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package dio.spring_security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/managers").hasRole("MANAGERS")
+                        .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                        .anyRequest().authenticated()
+                )
+                .formLogin();
+
+
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Criação dos usuários em memória
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        userDetailsService.createUser(
+                User.withUsername("tiago")
+                        .password("{noop}1234")
+                        .roles("USERS")
+                        .build());
+
+        userDetailsService.createUser(
+                User.withUsername("henrique")
+                        .password("{noop}12345")
+                        .roles("MANAGERS")
+                        .build());
+
+        return userDetailsService;
+    }
+}
